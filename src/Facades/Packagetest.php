d560d27aceb59f03cf05470f062aab03ccfb412f
小谢童鞋 <chuxinone@qq.com>
2022-09-25 01:31:14 +0800

    完整初次提交

---
+++ src/Facades/Packagetest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Aex\Packagetest\Facades;
+use Illuminate\Support\Facades\Facade;
+class Packagetest extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'packagetest';
+    }
+}
